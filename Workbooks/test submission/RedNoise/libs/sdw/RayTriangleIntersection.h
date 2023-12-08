d119318839858c1afebdf6a7b51cdfe69584ad8b
Keli202 <98463159+Keli202@users.noreply.github.com>
2023-12-08 08:56:07 +0000

    test submission

---
+++ Workbooks/test submission/RedNoise/libs/sdw/RayTriangleIntersection.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <iostream>
+#include "ModelTriangle.h"
+
+struct RayTriangleIntersection {
+
+    glm::vec3 intersectionPoint;
+    float distanceFromCamera;
+    ModelTriangle intersectedTriangle;
+    size_t triangleIndex;
+    float u;
+    float v;
+    glm::vec2 Texture;
+
+	RayTriangleIntersection();
+	RayTriangleIntersection(const glm::vec3 &point, float distance, const ModelTriangle &triangle, size_t index,float u,float v);
+	friend std::ostream &operator<<(std::ostream &os, const RayTriangleIntersection &intersection);
+};
